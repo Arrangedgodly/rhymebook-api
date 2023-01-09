ccc23a8b939f726bdcfb0cb557577f98680e1f39
Arrangedgodly <90996993+Arrangedgodly@users.noreply.github.com>
2023-01-08 21:16:59 -0700

    refactor: changing notes submission

--- routes/notes.js
+++ routes/notes.js
@@ -3,7 +3,7 @@ const { getNotes, deleteNote, patchNote } = require('../controllers/notes');
 const auth = require('../middlewares/auth');
 
 router.get('/:owner', auth, getNotes);
-router.patch('/:title', auth, patchNote);
-router.delete('/:title', auth, deleteNote);
+router.patch('/', auth, patchNote);
+router.delete('/', auth, deleteNote);
 
 module.exports = router;
